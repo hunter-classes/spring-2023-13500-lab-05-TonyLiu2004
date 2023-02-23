5d31deaa9b19535908a9ed8618a5bbe41c0dc6eb
TonyLiu <ltsplice@gmail.com>
2023-02-23 17:27:46 -0500

    d

--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,9 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "funcs.h"
+
+TEST_CASE("isDivisibleBy"){
+    CHECK(isDivisibleBy(10,15) == true);
+    CHECK(isDivisibleBy(20,3) == true);
+    CHECK(isDivisibleBy(126,252) == true);
+}
