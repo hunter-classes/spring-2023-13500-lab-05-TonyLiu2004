6885428a99b32f05bf51c1537ee0c88c20b1b9a0
TonyLiu <ltsplice@gmail.com>
2023-02-23 15:02:15 -0500

    lab 5 start

--- funcs.h
+++ funcs.h
@@ -1,3 +1,9 @@
 #pragma once
 
-// add prototypes here
+bool isDivisibleBy(int, int);
+bool isPrime(int);
+int nextPrime(int);
+int countPrimes(int, int);
+bool isTwinPrime(int);
+int nextTwinPrime(int);
+int largestTwinPrime(int, int);
