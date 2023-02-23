65fb16adfe4190c119e2c4b47a4c63758e3539f6
TonyLiu <ltsplice@gmail.com>
2023-02-23 17:03:33 -0500

    a

--- tests.cpp
+++ tests.cpp
@@ -2,14 +2,6 @@
 #include "doctest.h"
 #include "funcs.h"
 
-bool isDivisibleBy(int, int);
-bool isPrime(int);
-int nextPrime(int);
-int countPrimes(int, int);
-bool isTwinPrime(int);
-int nextTwinPrime(int);
-int largestTwinPrime(int, int);
-
 TEST_CASE("w"){
-    CHECK(isPrime(7) == 1);
+    
 }
